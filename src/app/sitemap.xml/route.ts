75d75a2f9405bb76d3ba21ae23d768577fb8d7a8
gihan12347 <pysicaltrainerweb@gmail.com>
2025-08-30 23:48:49 +0530

    added robot.txt file and sitemap.xml file

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const baseUrl = "https://freeonlinetextcomparetool.com"; 
+
+  // Example pages, you can generate dynamically from your data
+  const pages = ["", "compare-file","compare-images", "about", "feedback", "privacy-policy"];
+
+  const urls = pages
+    .map(
+      (page) => `
+    <url>
+      <loc>${baseUrl}/${page}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `
+    )
+    .join("");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+  </urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
